3f4ad7c8c9b7ae3a7db9801a1b321e3bcbcb0bec
Archana_Mishra21 <archanamishra.m7@gmail.com>
2026-07-11 14:27:27 +0530

    complete HR and Payroll modules with PDF Payslip

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request, redirect
-from models import db, Employee
-from models import Product , Attendance
+from models import db, Employee, SalaryStructure
+from models import Product , Attendance, Payroll
+from datetime import datetime
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import inch
+from flask import send_file
+from io import BytesIO
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import inch
+
+
+
 
 app = Flask(__name__)
 
@@ -34,21 +48,61 @@ def home():
     )
 
 
+
 @app.route("/add_employee", methods=["GET", "POST"])
 def add_employee():
 
     if request.method == "POST":
 
-        name = request.form["employee_name"]
-        department = request.form["department"]
-        salary = request.form["salary"]
-        email = request.form["email"]
-
         new_employee = Employee(
-            name=name,
-            department=department,
-            salary=salary,
-            email=email
+
+            employee_code=request.form["employee_code"],
+
+            first_name=request.form["first_name"],
+
+            last_name=request.form["last_name"],
+
+            father_name=request.form["father_name"],
+
+            gender=request.form["gender"],
+
+            dob=datetime.strptime(
+                request.form["dob"],
+                "%Y-%m-%d"
+            ).date() if request.form["dob"] else None,
+
+            phone=request.form["phone"],
+
+            email=request.form["email"],
+
+            address=request.form["address"],
+
+            city=request.form["city"],
+
+            state=request.form["state"],
+
+            pincode=request.form["pincode"],
+
+            aadhaar=request.form["aadhaar"],
+
+            pan=request.form["pan"],
+
+            bank_name=request.form["bank_name"],
+
+            account_number=request.form["account_number"],
+
+            ifsc=request.form["ifsc"],
+
+            department=request.form["department"],
+
+            designation=request.form["designation"],
+
+            joining_date=datetime.strptime(
+                request.form["joining_date"],
+                "%Y-%m-%d"
+            ).date() if request.form["joining_date"] else None,
+
+            status=request.form["status"]
         )
 
         db.session.add(new_employee)
@@ -57,6 +111,7 @@ def add_employee():
         return redirect("/employees")
 
     return render_template("add_employee.html")
+
 
 
 @app.route("/employees")
@@ -75,6 +130,17 @@ def employees():
         "employee.html",
         employees=employees,
         search=search
+    )
+
+
+@app.route("/employee/<int:id>")
+def employee_profile(id):
+
+    employee = Employee.query.get_or_404(id)
+
+    return render_template(
+        "employee_profile.html",
+        employee=employee
     )
 
 @app.route("/delete_employee/<int:id>")
@@ -266,6 +332,344 @@ def edit_attendance(id):
         "edit_attendance.html",
         record=record,
         employees=employees
+    )
+
+@app.route("/salary")
+def salary():
+
+    salaries = SalaryStructure.query.all()
+
+    return render_template(
+        "salary.html",
+        salaries=salaries
+    )
+
+@app.route("/add_salary", methods=["GET", "POST"])
+def add_salary():
+
+    employees = Employee.query.all()
+
+    if request.method == "POST":
+
+        salary = SalaryStructure(
+
+            employee_id=request.form["employee_id"],
+
+            basic=request.form["basic"],
+
+            hra=request.form["hra"],
+
+            fuel_allowance=request.form["fuel_allowance"],
+
+            medical_allowance=request.form["medical_allowance"],
+
+            travel_allowance=request.form["travel_allowance"],
+
+            overtime=request.form["overtime"],
+
+            bonus=request.form["bonus"],
+
+            other_allowance=request.form["other_allowance"]
+
+        )
+
+        db.session.add(salary)
+
+        db.session.commit()
+
+        return redirect("/salary")
+
+    return render_template(
+        "add_salary.html",
+        employees=employees
+    )
+
+@app.route("/payroll")
+def payroll():
+
+    payrolls = Payroll.query.all()
+
+    return render_template(
+        "payroll.html",
+        payrolls=payrolls
+    )
+
+
+@app.route("/generate_payroll", methods=["GET", "POST"])
+def generate_payroll():
+
+    employees = Employee.query.all()
+
+    if request.method == "POST":
+        employee_id = request.form["employee_id"]
+        month = request.form["month"]
+        year = int(request.form["year"])
+        advance = float(request.form["advance"])
+        pf = float(request.form["pf"])
+        esi = float(request.form["esi"])
+        professional_tax = float(request.form["professional_tax"])
+        other_deduction = float(request.form["other_deduction"])
+        salary = SalaryStructure.query.filter_by(
+            employee_id=employee_id
+        ).first()
+
+        if salary is None:
+
+            return "Salary Structure not found for this employee."
+
+        gross_salary = (
+            salary.basic
+            + salary.hra
+            + salary.fuel_allowance
+            + salary.medical_allowance
+            + salary.travel_allowance
+            + salary.overtime
+            + salary.bonus
+            + salary.other_allowance
+        )
+
+        total_deduction = (
+            advance
+            + pf
+            + esi
+            + professional_tax
+            + other_deduction
+        )
+
+        net_salary = gross_salary - total_deduction
+
+        payroll = Payroll(
+            employee_id=employee_id,
+            month=month,
+            year=year,
+            present_days=30,
+            leave_days=0,
+            gross_salary=gross_salary,
+            advance=advance,
+            pf=pf,
+            esi=esi,
+            professional_tax=professional_tax,
+            other_deduction=other_deduction,
+            total_deduction=total_deduction,
+            net_salary=net_salary,
+            payment_status="Pending"
+        )
+
+        db.session.add(payroll)
+        db.session.commit()
+        return redirect("/payroll")
+
+    return render_template(
+        "generate_payroll.html",
+        employees=employees
+    )
+
+@app.route("/payslip/<int:id>")
+def payslip(id):
+
+    payroll = Payroll.query.get_or_404(id)
+
+    salary = SalaryStructure.query.filter_by(
+        employee_id=payroll.employee_id
+    ).first()
+
+    return render_template(
+        "payslip.html",
+        payroll=payroll,
+        salary=salary
+    )
+
+
+@app.route("/download_payslip/<int:id>")
+def download_payslip(id):
+
+    payroll = Payroll.query.get_or_404(id)
+
+    salary = SalaryStructure.query.filter_by(
+        employee_id=payroll.employee_id
+    ).first()
+
+    buffer = BytesIO()
+
+    doc = SimpleDocTemplate(buffer)
+
+    styles = getSampleStyleSheet()
+
+    elements = []
+
+    # ---------------------------
+    # Title
+    # ---------------------------
+
+    elements.append(
+        Paragraph(
+            "<b><font size='20'>WORKSPHERE ERP</font></b>",
+            styles["Title"]
+        )
+    )
+
+    elements.append(
+        Paragraph(
+            "<b>Employee Payslip</b>",
+            styles["Heading2"]
+        )
+    )
+
+    elements.append(
+        Paragraph("<br/>", styles["Normal"])
+    )
+
+    # ---------------------------
+    # Employee Details
+    # ---------------------------
+
+    employee_table = Table([
+
+        ["Employee",
+         payroll.employee.first_name + " " + payroll.employee.last_name],
+
+        ["Employee Code",
+         payroll.employee.employee_code],
+
+        ["Department",
+         payroll.employee.department],
+
+        ["Designation",
+         payroll.employee.designation],
+
+        ["Month",
+         payroll.month],
+
+        ["Year",
+         str(payroll.year)]
+
+    ], colWidths=[2.3*inch, 4*inch])
+
+    employee_table.setStyle(TableStyle([
+
+        ('GRID',(0,0),(-1,-1),1,colors.grey),
+
+        ('BACKGROUND',(0,0),(0,-1),colors.HexColor("#EAF4FF")),
+
+        ('FONTNAME',(0,0),(-1,-1),"Helvetica"),
+
+        ('BOTTOMPADDING',(0,0),(-1,-1),8)
+
+    ]))
+
+    elements.append(employee_table)
+
+    elements.append(
+        Paragraph("<br/>", styles["Normal"])
+    )
+
+    # ---------------------------
+    # Earnings
+    # ---------------------------
+
+    gross = payroll.gross_salary
+
+    earnings = [
+
+        ["Earnings","Amount"],
+
+        ["Basic Salary", salary.basic],
+
+        ["HRA", salary.hra],
+
+        ["Fuel Allowance", salary.fuel_allowance],
+
+        ["Medical Allowance", salary.medical_allowance],
+
+        ["Travel Allowance", salary.travel_allowance],
+
+        ["Overtime", salary.overtime],
+
+        ["Bonus", salary.bonus],
+
+        ["Other Allowance", salary.other_allowance],
+
+        ["Gross Salary", gross]
+
+    ]
+
+    earnings_table = Table(earnings)
+
+    earnings_table.setStyle(TableStyle([
+
+        ('GRID',(0,0),(-1,-1),1,colors.black),
+
+        ('BACKGROUND',(0,0),(-1,0),colors.HexColor("#198754")),
+
+        ('TEXTCOLOR',(0,0),(-1,0),colors.white),
+
+        ('BACKGROUND',(0,-1),(-1,-1),colors.HexColor("#D1F7D6")),
+
+        ('FONTNAME',(0,0),(-1,-1),"Helvetica-Bold")
+
+    ]))
+
+    elements.append(earnings_table)
+
+    elements.append(
+        Paragraph("<br/>", styles["Normal"])
+    )
+
+    # ---------------------------
+    # Deductions
+    # ---------------------------
+
+    deductions = [
+
+        ["Deductions","Amount"],
+
+        ["Advance", payroll.advance],
+
+        ["PF", payroll.pf],
+
+        ["ESI", payroll.esi],
+
+        ["Professional Tax", payroll.professional_tax],
+
+        ["Other Deduction", payroll.other_deduction],
+
+        ["Total Deduction", payroll.total_deduction],
+
+        ["Net Salary", payroll.net_salary]
+
+    ]
+
+    deduction_table = Table(deductions)
+
+    deduction_table.setStyle(TableStyle([
+
+        ('GRID',(0,0),(-1,-1),1,colors.black),
+
+        ('BACKGROUND',(0,0),(-1,0),colors.HexColor("#DC3545")),
+
+        ('TEXTCOLOR',(0,0),(-1,0),colors.white),
+
+        ('BACKGROUND',(0,-1),(-1,-1),colors.HexColor("#FFF3CD")),
+
+        ('FONTNAME',(0,0),(-1,-1),"Helvetica-Bold")
+
+    ]))
+
+    elements.append(deduction_table)
+
+    doc.build(elements)
+
+    buffer.seek(0)
+
+    filename = f"Payslip_{payroll.employee.employee_code}_{payroll.month}_{payroll.year}.pdf"
+
+    return send_file(
+
+        buffer,
+        as_attachment=True,
+        download_name=filename,
+        mimetype="application/pdf"
+
     )
 
 
--- models.py
+++ models.py
@@ -2,25 +2,168 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+
 class Employee(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+
+    employee_code = db.Column(db.String(20), unique=True, nullable=False)
+
+    photo = db.Column(db.String(200), default="default.png")
+
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+
+    father_name = db.Column(db.String(100))
+
+    gender = db.Column(db.String(20))
+
+    dob = db.Column(db.Date)
+
+    phone = db.Column(db.String(15), unique=True)
+
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+    address = db.Column(db.String(300))
+    city = db.Column(db.String(100))
+    state = db.Column(db.String(100))
+    pincode = db.Column(db.String(10))
+
+    aadhaar = db.Column(db.String(12), unique=True)
+
+    pan = db.Column(db.String(10), unique=True)
+
+    bank_name = db.Column(db.String(100))
+    account_number = db.Column(db.String(30))
+    ifsc = db.Column(db.String(20))
+
     department = db.Column(db.String(100))
-    salary = db.Column(db.Integer)
-    email = db.Column(db.String(100))
+    designation = db.Column(db.String(100))
 
-class Product(db.Model):
+    joining_date = db.Column(db.Date)
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    category = db.Column(db.String(100))
-    quantity = db.Column(db.Integer)
-    price = db.Column(db.Integer)
-    supplier = db.Column(db.String(100))
+    status = db.Column(db.String(30), default="Active")
+
+
+
 
 class Attendance(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    employee_id = db.Column(db.Integer)
-    date = db.Column(db.String(50))
+
+    employee_id = db.Column(
+        db.Integer,
+        db.ForeignKey("employee.id"),
+        nullable=False
+    )
+
+    date = db.Column(db.Date)
+
     status = db.Column(db.String(20))
+
+    employee = db.relationship("Employee")
+
+
+
+
+class SalaryStructure(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    employee_id = db.Column(
+        db.Integer,
+        db.ForeignKey("employee.id"),
+        nullable=False
+    )
+
+    basic = db.Column(db.Float, default=0)
+
+    hra = db.Column(db.Float, default=0)
+
+    fuel_allowance = db.Column(db.Float, default=0)
+
+    medical_allowance = db.Column(db.Float, default=0)
+
+    travel_allowance = db.Column(db.Float, default=0)
+
+    overtime = db.Column(db.Float, default=0)
+
+    bonus = db.Column(db.Float, default=0)
+
+    other_allowance = db.Column(db.Float, default=0)
+
+    employee = db.relationship("Employee")
+
+
+
+
+class Payroll(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    employee_id = db.Column(
+        db.Integer,
+        db.ForeignKey("employee.id"),
+        nullable=False
+    )
+
+    month = db.Column(db.String(20))
+
+    year = db.Column(db.Integer)
+
+    present_days = db.Column(db.Integer, default=0)
+
+    leave_days = db.Column(db.Integer, default=0)
+
+    gross_salary = db.Column(db.Float, default=0)
+
+    advance = db.Column(db.Float, default=0)
+
+    pf = db.Column(db.Float, default=0)
+
+    esi = db.Column(db.Float, default=0)
+
+    professional_tax = db.Column(db.Float, default=0)
+
+    other_deduction = db.Column(db.Float, default=0)
+
+    total_deduction = db.Column(db.Float, default=0)
+
+    net_salary = db.Column(db.Float, default=0)
+
+    payment_status = db.Column(
+        db.String(20),
+        default="Pending"
+    )
+
+    employee = db.relationship("Employee")
+
+
+
+
+class Product(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    item_code = db.Column(db.String(20), unique=True)
+
+    name = db.Column(db.String(100), nullable=False)
+
+    category = db.Column(db.String(100))
+
+    brand = db.Column(db.String(100))
+
+    unit = db.Column(db.String(20))
+
+    quantity = db.Column(db.Integer)
+
+    minimum_stock = db.Column(db.Integer)
+
+    purchase_price = db.Column(db.Float)
+
+    selling_price = db.Column(db.Float)
+
+    gst = db.Column(db.Float)
+
+    supplier = db.Column(db.String(100))
